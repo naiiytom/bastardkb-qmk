d01cee6fedbe4ce3f328b62b91e53646b7015685
Yuttapichai <y.lamnaonan@gmail.com>
2024-03-19 10:50:12 +0700

    update new keymaps for my scylla build

--- keyboards/bastardkb/scylla/keymaps/naiiytom/keymap.c
+++ keyboards/bastardkb/scylla/keymaps/naiiytom/keymap.c
@@ -22,16 +22,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_4x6_5(
         KC_ESC,  KC_1, KC_2, KC_3,    KC_4,    KC_5,       KC_6,   KC_7,    KC_8,    KC_9,   KC_0,    KC_GRV,
         KC_TAB,  KC_Q, KC_W, KC_E,    KC_R,    KC_T,       KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_MINS,
-        KC_LALT, KC_A, KC_S, KC_D,    KC_F,    KC_G,       KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+        KC_RALT, KC_A, KC_S, KC_D,    KC_F,    KC_G,       KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z, KC_X, KC_C,    KC_V,    KC_B,       KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
                              KC_LCTL, KC_LBRC, MO(1),      MO(2),  KC_RBRC, KC_RGUI, 
                                       KC_DEL,  KC_SPC,     KC_ENT, KC_BSPC
         ),
     [1] = LAYOUT_split_4x6_5(
-        _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, KC_VOLU,       KC_BRID, KC_BRIU, _______, _______, _______, _______,
+        KC_MUTE, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, KC_VOLU,       KC_BRID, KC_BRIU, _______, _______, KC_CALC, KC_DEL,
         KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                   KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
-        _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END, _______,        KC_P1, KC_P2, KC_P3, KC_P4, KC_P5, KC_BSLS,
-        _______, KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, _______,       KC_P6, KC_P7, KC_P8, KC_P9, KC_P0, KC_PIPE,
+        KC_LCAP, KC_HOME, KC_PGUP, KC_PGDN, KC_END, _______,        KC_P1, KC_P2, KC_P3, KC_P4, KC_P5, KC_BSLS,
+        _______, KC_MYCM, KC_CPNL, _______, _______, _______,       KC_P6, KC_P7, KC_P8, KC_P9, KC_P0, KC_PIPE,
                                     _______, _______, _______,      _______, _______, _______, 
                                                 _______, _______,   _______, _______
         ),
